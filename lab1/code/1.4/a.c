ec5bc0014a0616d7f3b17f1fcac83df0677aca82
Darya Evdokimova <evddaryaevg@gmail.com>
2023-10-07 13:05:52 +0700

    added 1.4 a, b

---
+++ lab1/code/1.4/a.c
@@ -0,0 +1,29 @@
+#define _GNU_SOURCE
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void *routine(void *args) {
+  while (1) {
+    printf("This is mythread!\n");
+  }
+}
+
+int main() {
+  pthread_t tid;
+
+  if (pthread_create(&tid, NULL, routine, NULL) != 0) {
+    perror("pthread_create() error");
+    return -1;
+  }
+
+  if (pthread_cancel(tid) != 0) {
+    perror("pthread_cancel() error");
+    return -1;
+  }
+
+  pthread_exit(NULL);
+}
---
+++ lab1/code/1.4/b.c
@@ -0,0 +1,35 @@
+#define _GNU_SOURCE
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void *routine(void *args) {
+  // if (pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL) != 0) {
+  //   perror("pthread_setcanceltype() error");
+  //   return NULL;
+  // }
+
+  int counter = 0;
+  while (1) {
+    counter++;
+  }
+}
+
+int main() {
+  pthread_t tid;
+
+  if (pthread_create(&tid, NULL, routine, NULL) != 0) {
+    perror("pthread_create() error");
+    return -1;
+  }
+
+  if (pthread_cancel(tid) != 0) {
+    perror("pthread_cancel() error");
+    return -1;
+  }
+
+  pthread_exit(NULL);
+}
